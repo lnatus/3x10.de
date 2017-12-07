c1e3dcf29fc66be3f0215796aea505c0202c2527
= <l.natus@live.com>
2017-12-07 08:09:24 +0100

    Add blog post 1

--- src/scripts/app/ts/db/posts.ts
+++ src/scripts/app/ts/db/posts.ts
@@ -7,7 +7,7 @@ namespace X10 {
       private p1 = new X10.Model.Post('2017_12-05-P1','http://google.de','First Post','My First Post Teaser','images/2017/12/1.png', new Date(), ['Bodybuilding', 'Fitness'])
       private p2 = new X10.Model.Post('2017-12-05-P2','http://google.de','Second Post','My Second Post Teaser','images/2017/12/1.png', new Date(), ['Bodybuilding', 'Fitness'])
 
-      public  getLatest(): X10.Model.Post[] {
+      public getLatest(): X10.Model.Post[] {
          this.latest.push(this.p1, this.p2)
          return this.latest
       }
